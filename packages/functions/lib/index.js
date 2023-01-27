6056c57de09b8e63a154eb44136073089244cfb1
Mike Rudenko <mihailo.rudenko.main@gmail.com>
2023-01-27 19:54:00 +0200

    code refactoring

--- packages/functions/lib/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const admin = require("firebase-admin");
-var functions_users_1 = require("./functions.users");
-exports.onUserCreate = functions_users_1.onUserCreate;
-exports.updateUser = functions_users_1.updateUser;
-admin.initializeApp();
-//# sourceMappingURL=index.js.map
